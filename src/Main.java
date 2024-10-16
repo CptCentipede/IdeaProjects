496a7be2b30fb657f5ffedcfd5053b1cff28028b
Miles <milesgriffin63@gmail.com>
2024-10-16 17:32:53 -0600

    opened all files

--- src/Main.java
+++ src/Main.java
@@ -1,112 +1,103 @@
 /*
 Name: Miles Aether
-Date: 10/09/2024
+Date: 10/20/2024
 Class: CSCI 2251
-Purpose: This program will take in a file name from the command line to take in a file that will then be organized
-         in order, without duplicates, to be written to an output for both the measurements given and converted into
-         the imperial measuring system
-Resources: I used the following to get the filename from the command line argument
-           https://stackoverflow.com/questions/16802147/java-i-want-to-read-a-file-name-from-command-line-then-use-a-bufferedreader-to
-           I used the following to understand the compareTo method where the class implements Comparable
-           https://java-programming.mooc.fi/part-10/2-interface-comparable
-           I used the following to figure out how to return a string with two decimals if the decimals after height or weight were not zero
-           https://stackoverflow.com/questions/14799943/how-to-check-if-number-is-a-decimal
+Purpose: Doubly-linked list that sorts itself to be used in place of an arraylist
 */
 
-/*
-Q1: Car and Engine are related by which, Inheritance or Composition?
-A car has an engine, so composition
 
-Q2: Color and Red are related by which, Inheritance or Composition?
-Red is a color, so inheritance
-
-Q3: Shirt and Clothing are related by which, Inheritance or Composition?
-Shirt is a type of clothing, so inheritance
-
-Q4: Furniture and Desk are related by which, Inheritance or Composition?
-Desk is a type of furniture, so inheritance
-
-Q5: CellPhone and Battery are related by which, Inheritance or Composition?
-A cellphone has a battery, so composition
-*/
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.File;
+import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args)
     {
-        //instantiate imperial and ordered classes
-        PersonList imperialSet = new PersonImperialSet();
-        PersonList orderedSet = new PersonOrderedSet();
+        //create arrayList that will save the data from hurricane objects
+        ArrayList<HurricaneRowData> hurricaneData = new ArrayList<HurricaneRowData>();
 
-        //use try catch for file input
-        try {
-            //create a basic file that will be read if there are no arguments on the command prompt
-            File hrFile = new File("test.txt");
-
-            //take an argument from the command prompt for the file name
-            if (0 < args.length) {
-                String filename = args[0];
-                hrFile = new File(filename);
-            }
-
+        //use try and catch to see if file exists
+        try
+        {
             //add file and create a scanner that will read the info from that file
-            Scanner fileReader = new Scanner(hrFile);
+            File aceFile = new File("ace.csv");
+            Scanner fileInput = new Scanner(aceFile);
 
             //Strip first line
-            fileReader.nextLine();
+            fileInput.nextLine();
 
             //while the file has more data keep reading the data
-            while (fileReader.hasNext()) {
+            while(fileInput.hasNext())
+            {
                 //take each line, store into an array without commas, and create array to store the data as integers
-                String name = fileReader.next();
-                double height = fileReader.nextDouble();
-                double weight = fileReader.nextDouble();
+                String line = fileInput.nextLine();
+                String[] values = line.split(",");
+                int[] valueAsInt = new int[5];
 
+                //loop through the array of strings and parse them into an array of integers
+                for(int i=0; i<values.length; i++)
+                {
+                    int stringToInt = Integer.parseInt(values[i]);
+                    valueAsInt[i] = stringToInt;
+                }
 
-                //create an object of type Person for the imperial and ordered set
-                Person imperialPerson = new Person(name, height, weight);
-                Person orderedPerson = new Person(name, height, weight);
+                //create a new data of HurricaneRowData type then add it to the arrayList
+                HurricaneRowData dataForRow = new HurricaneRowData(valueAsInt[0], valueAsInt[1], valueAsInt[2], valueAsInt[3], valueAsInt[4]);
+                hurricaneData.add(dataForRow);
 
-                //add the person to the imperial and ordered classes
-                imperialSet.add(imperialPerson);
-                orderedSet.add(orderedPerson);
-            }   //end of while loop
+            }//while loop
 
-            //print imperial and ordered sets to command line
-            System.out.println(imperialSet.toString()+"\n");
-            System.out.println(orderedSet.toString());
+            //print out the max value
+            System.out.println(findAceMaxYear(hurricaneData));
 
             //close the scanner that reads data from file
-            fileReader.close();
-        }   //end of try
+            fileInput.close();
+        }
         catch(FileNotFoundException e)
         {
             System.out.println("sorry your file cannot be found");
-        }   //end of catch
+        }
 
-        //write a file for output using try catch
-		try
-		{
-            //write a file for the PersonImperialSet
-			FileWriter writeImperial = new FileWriter("hr_imperial_set_output.txt");
-            writeImperial.write(imperialSet.toString());
-            writeImperial.close();
+        //write the file output for the year with the max ACE
+        try
+        {
+            //create a file writer and write to an output file
+            FileWriter writeFile = new FileWriter("output.txt");
+            int max = findAceMaxYear(hurricaneData);
+            String strMax = Integer.toString(max);
+            writeFile.write(strMax);
 
-            //write a file for the PersonOrderedSet
-            FileWriter writeOrdered = new FileWriter("outputhr_ordered_set_output.txt");
-            writeOrdered.write(orderedSet.toString());
-            writeOrdered.close();
-		}   //end of try
-		catch(IOException e)
-		{
-			e.printStackTrace();
-			System.out.println(e);
-			System.exit(1);
-		}   //end of catch
-    }   //end of main
-}   //end of class
+            //close the file writer
+            writeFile.close();
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+    //method that finds the highest ACE index
+    private static int findAceMaxYear(ArrayList<HurricaneRowData> hurricaneData)
+    {
+        //create data of type HurricaneRowData and an integer to store the value the max is at
+        HurricaneRowData max = hurricaneData.get(0);
+        int maxValue = 0;
+
+        //loop through the second column of the object to find the largest value then set that to the max value
+        for(int i=0; i<hurricaneData.size(); i++)
+        {
+            if(hurricaneData.get(i).getace() > max.getace())
+            {
+                max = hurricaneData.get(i);
+                maxValue = Integer.valueOf(max.getyear());
+            }   //end if
+        }   //end for
+
+        //return the maximum value
+        return maxValue;
+    }   //end findAceYearMax
+}   //end class
