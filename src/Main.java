48afa9111982548b603fda62636924cf5f15fca6
Miles <milesgriffin63@gmail.com>
2024-10-25 12:23:32 -0600

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,81 @@
-/*
-Name: Miles Aether
-Date: 10/20/2024
-Class: CSCI 2251
-Purpose: Doubly-linked list that sorts itself to be used in place of an arraylist
-Resources: I used the following for the insert method, but left the code as is because changing it didn't make it work
-           https://www.geeksforgeeks.org/insertion-sort-doubly-linked-list/
-*/
-
-import java.util.Scanner;
 import java.io.File;
-import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
-        //create linkedList that will save the data from hurricane objects
-        DoublyLinkedSortedList data = new DoublyLinkedSortedList();
+        //instantiate imperial and ordered classes
+        //SortedTreeSetInterface imperialSet = new SortedTreeSet();
+        SortedTreeSetInterface orderedSet = new SortedTreeSet();
 
-        //use try and catch to see if file exists
-        try
-        {
+        //use try catch for file input
+        try {
+            //create a basic file that will be read if there are no arguments on the command prompt
+            File hrFile = new File("test.txt");
+
+            //take an argument from the command prompt for the file name
+            if (0 < args.length) {
+                String filename = args[0];
+                hrFile = new File(filename);
+            }
+
             //add file and create a scanner that will read the info from that file
-            File aceFile = new File("ace.csv");
-            Scanner fileInput = new Scanner(aceFile);
+            Scanner fileReader = new Scanner(hrFile);
 
             //Strip first line
-            fileInput.nextLine();
+            fileReader.nextLine();
 
             //while the file has more data keep reading the data
-            while(fileInput.hasNext())
-            {
+            while (fileReader.hasNext()) {
                 //take each line, store into an array without commas, and create array to store the data as integers
-                String line = fileInput.nextLine();
-                String[] values = line.split(",");
-                int[] valueAsInt = new int[5];
+                String name = fileReader.next();
+                double height = fileReader.nextDouble();
+                double weight = fileReader.nextDouble();
 
-                //loop through the array of strings and parse them into an array of integers
-                for(int i=0; i<values.length; i++)
-                {
-                    int stringToInt = Integer.parseInt(values[i]);
-                    valueAsInt[i] = stringToInt;
-                }
 
-                //create a new data of HurricaneRowData type then add it to the linkedList
-                HurricaneRowData dataForRow = new HurricaneRowData(valueAsInt[0], valueAsInt[1], valueAsInt[2], valueAsInt[3], valueAsInt[4]);
-                data.insert(dataForRow);
-            }//while loop
+                //create an object of type Person for the imperial and ordered set
+                //Person imperialPerson = new Person(name, height, weight);
+                Person newPerson = new Person(name, height, weight);
+
+                //add the person to the imperial and ordered classes
+                //imperialSet.add(imperialPerson);
+                orderedSet.add(newPerson);
+            }   //end of while loop
+
+            //print imperial and ordered sets to command line
+            //System.out.println(imperialSet.toString()+"\n");
+            System.out.println(orderedSet.toString());
 
             //close the scanner that reads data from file
-            fileInput.close();
-        }   //end try
+            fileReader.close();
+        }   //end of try
         catch(FileNotFoundException e)
         {
             System.out.println("sorry your file cannot be found");
-        }   //end catch
+        }   //end of catch
 
-        //write the file output for the year with the max ACE
+        //write a file for output using try catch
         try
         {
-            //create a file writer and write to an output file
-            FileWriter writeFile = new FileWriter("output.txt");
-            int max = findAceMaxYear(data);
-            String strMax = Integer.toString(max);
-            writeFile.write(strMax);
+            //write a file for the PersonImperialSet
+            /*FileWriter writeImperial = new FileWriter("hr_imperial_set_output.txt");
+            writeImperial.write(imperialSet.toString());
+            writeImperial.close();*/
 
-            //close the file writer
-            writeFile.close();
-        }   //end try
+            //write a file for the PersonOrderedSet
+            FileWriter writeOrdered = new FileWriter("tree_output.txt");
+            writeOrdered.write(orderedSet.toString());
+            writeOrdered.close();
+        }   //end of try
         catch(IOException e)
         {
+            e.printStackTrace();
             System.out.println(e);
-        }   //end catch
-
-    }
-
-    //method that finds the highest ACE index
-    private static int findAceMaxYear(DoublyLinkedSortedList data)
-    {
-        //create data of type HurricaneRowData and an integer to store the value the max is at
-        DoublyLinkedSortedList link = data.getFirst();
-        HurricaneRowData dat = link.getValue();
-
-        //get the year for the value
-        int max_year = dat.getYear();
-
-        //print it out
-        System.out.println("Year of max ace: "+max_year);
-        System.out.println("All data in order of Ace:");
-        System.out.println(data);
-
-        //return the max ace year
-        return max_year;
-    }   //end findAceYearMax
-}   //end class
+            System.exit(1);
+        }   //end of catch
+    }   //end of main
+}   //end of class
---
+++ src/SortedTreeSet.java
@@ -0,0 +1,44 @@
+public class SortedTreeSet implements SortedTreeSetInterface
+{
+    public Person getPerson()
+    {
+
+    }
+
+    public boolean hasLeft()
+    {
+        return true;    //PLACEHOLDER
+    }
+    public void setLeft(SortedTreeSet left)
+    {
+
+    }
+    public SortedTreeSet getLeft()
+    {
+
+    }
+
+    public boolean hasRight()
+    {
+        return true;    //PLACEHOLDER
+    }
+    public void setRight(SortedTreeSet right)
+    {
+
+    }
+    public SortedTreeSet getRight()
+    {
+
+    }
+
+    public void add(Person p)
+    {
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "";  //PLACEHOLDER
+    }
+}
