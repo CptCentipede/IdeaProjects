6cff6446d5bd236c4f66dd7d3f6dce19797df27a
Miles <milesgriffin63@gmail.com>
2024-11-03 13:49:53 -0700

    Create references for the four quadrants and fill in the parameters for the four threads

--- src/Main.java
+++ src/Main.java
@@ -42,41 +42,67 @@ public class Main
             //add file and create a scanner that will read the info from that file
             Scanner fileReader = new Scanner(matrixFile);
 
-            //while the file has more data keep reading the data
-            while (fileReader.hasNext())
+            //read the dimensions for the number of rows and columns
+            int numRows = fileReader.nextInt();
+            int numCols = fileReader.nextInt();
+
+            //create two matrices to store the data
+            int[][] matrix1 = new int[numRows][numCols];
+            int[][] matrix2 = new int[numRows][numCols];
+
+            //populate the first matrix
+            //first the rows
+            for(int i=0; i<numRows; i++)
             {
-                //read the dimensions for the number of rows and columns
-                int numRows = fileReader.nextInt();
-                int numCols = fileReader.nextInt();
-
-                //create two matrices to store the data
-                int[][] matrix1 = new int[numRows][numCols];
-                int[][] matrix2 = new int[numRows][numCols];
-
-                //populate the first matrix
-                //first the rows
-                for(int i=0; i<numRows; i++)
+                //then the columns
+                for(int j=0; j<numCols; j++)
                 {
-                    //then the columns
-                    for(int j=0; j<numCols; j++)
-                    {
-                        //put the next integer at index [i][j]
-                        matrix1[i][j] = fileReader.nextInt();
-                    }   //end for loop cols
-                }   //end for loop rows
+                    //put the next integer at index [i][j]
+                    matrix1[i][j] = fileReader.nextInt();
+                }   //end for loop cols
+            }   //end for loop rows
 
-                //populate the second matrix
-                //first the rows
-                for(int i=numRows; i<(2*numRows); i++)
+            //populate the second matrix
+            //first the rows
+            for(int i=numRows; i<(2*numRows); i++)
+            {
+                //then the columns
+                for(int j=numCols; j<(2*numCols); j++)
                 {
-                    //then the columns
-                    for(int j=numCols; j<(2*numCols); j++)
-                    {
-                        //put the next integer at index [i][j]
-                        matrix2[i][j] = fileReader.nextInt();
-                    }   //end for loop cols
-                }   //end for loop rows
-            }   //end of while loop
+                    //put the next integer at index [i][j]
+                    matrix2[i][j] = fileReader.nextInt();
+                }   //end for loop cols
+            }   //end for loop rows
+
+            //create references to the four quadrants
+            String quad1 = "First Quadrant";
+            String quad2 = "Second Quadrant";
+            String quad3 = "Third Quadrant";
+            String quad4 = "Fourth Quadrant";
+
+            //create four threads that will add each quadrant concurrently
+            ThreadOperation thread1 = new ThreadOperation(matrix1, matrix2, quad1);
+            ThreadOperation thread2 = new ThreadOperation(matrix1, matrix2, quad2);
+            ThreadOperation thread3 = new ThreadOperation(matrix1, matrix2, quad3);
+            ThreadOperation thread4 = new ThreadOperation(matrix1, matrix2, quad4);
+
+            //start the threads
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread4.start();
+
+            //join threads so that they will complete before the rest of the program continnues
+            try{
+                thread1.join();
+                thread2.join();
+                thread3.join();
+                thread4.join();
+            }   //end try
+            catch(InterruptedException e)
+            {
+                System.out.println("Interrupted");
+            }   //end catch
 
             //close the scanner that reads data from file
             fileReader.close();
@@ -85,30 +111,6 @@ public class Main
         {
             System.out.println("sorry your file cannot be found");
         }   //end of catch
-
-        //create four threads that will add each quadrant concurrently
-        ThreadOperation thread1 = new ThreadOperation();
-        ThreadOperation thread2 = new ThreadOperation();
-        ThreadOperation thread3 = new ThreadOperation();
-        ThreadOperation thread4 = new ThreadOperation();
-
-        //start the threads
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-
-        //join threads so that they will complete before the rest of the program continnues
-        try{
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-        }   //end try
-        catch(InterruptedException e)
-        {
-            System.out.println("Interrupted");
-        }   //end catch
     }   //end main
 
     //method that prints out the 2D array
