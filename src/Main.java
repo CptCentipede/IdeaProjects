d9a9d4ed911e1a98907efb4ec363ac5d83bf4ff6
Miles <milesgriffin63@gmail.com>
2024-11-03 12:08:36 -0700

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,67 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+/*
+Name: Miles Aether
+Date: 11/03/2024
+Class: CSCI 2251
+Purpose: To demonstrate understanding of multithreading and its usefulness in splitting larger problems into smaller
+         ones as well as minimizing system resource usage
+
+Q1: One of the goals of multi-threading is to minimize the resource usage, such as memory and processor cycles.
+In three sentences, explain how multi-threaded code accomplishes this goal. Consider writing about blocking on I/O,
+multicore machines, how sluggish humans are, threads compared to processes, etcetera, and connect these issues to
+multi-threading.
+
+Since they can be run at the same times, threads will complete their tasks faster, running concurrently, rather than
+if they each ran one at a time. This also runs more efficiently because if your device has a 32 core CPU, that allows
+for the opportunity of 32 different threads that can be used to make the process potentially around 32 times quicker to
+complete. Since some threads can become blocked on certain IO operations, it is also important that the CPU switches
+to another thread, so that it is not utterly idle.
+*/
 import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main
 {
+    //should not need to use scanner for anything other than nextInt()
     public static void main(String[] args)
     {
-        //instantiate tree as a new object
-        SortedTreeSetInterface orderedSet = new SortedTreeSet();
+        //create four threads that will add each quadrant concurrently
+        ThreadOperation thread1 = new ThreadOperation();
+        ThreadOperation thread2 = new ThreadOperation();
+        ThreadOperation thread3 = new ThreadOperation();
+        ThreadOperation thread4 = new ThreadOperation();
 
-        //use try catch for file input
-        try {
-            //create a basic file that will be read if there are no arguments on the command prompt
-            File hrFile = new File("test.txt");
+        //start the threads
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
 
-            //take an argument from the command prompt for the file name
-            if (0 < args.length) {
-                String filename = args[0];
-                hrFile = new File(filename);
-            }
-
-            //add file and create a scanner that will read the info from that file
-            Scanner fileReader = new Scanner(hrFile);
-
-            //Strip first line
-            fileReader.nextLine();
-
-            //while the file has more data keep reading the data
-            while (fileReader.hasNext()) {
-                //take each line, store into an array without commas, and create array to store the data as integers
-                String name = fileReader.next();
-                double height = fileReader.nextDouble();
-                double weight = fileReader.nextDouble();
-
-
-                //create an object of type Person for the tree ordered set
-                Person newPerson = new Person(name, height, weight);
-
-                //add the person to the tree ordered set
-                orderedSet.add(newPerson);
-            }   //end of while loop
-
-            //print tree ordered set
-            System.out.println(orderedSet.toString());
-
-            //close the scanner that reads data from file
-            fileReader.close();
-        }   //end of try
-        catch(FileNotFoundException e)
+        //join threads so that they will complete before the rest of the program continnues
+        try{
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            thread4.join();
+        }   //end try
+        catch(InterruptedException e)
         {
-            System.out.println("sorry your file cannot be found");
-        }   //end of catch
+            System.out.println("Interrupted");
+        }   //end catch
+    }   //end main
 
-        //write a file for output using try catch
-        try
+    //method that prints out the 2D array
+    public void print2dArray(int[][] matrix)
+    {
+        //loop through the rows
+        for(int i=0; i<; i++)
         {
-            //write a file for the TreeOrderedSet
-            FileWriter writeOrdered = new FileWriter("tree_output.txt");
-            writeOrdered.write(orderedSet.toString());
-            writeOrdered.close();
-        }   //end of try
-        catch(IOException e)
-        {
-            e.printStackTrace();
-            System.out.println(e);
-            System.exit(1);
-        }   //end of catch
-    }   //end of main
-}   //end of class
+            //loop through the columns
+            for (int j = 0; j <; j++)
+            {
+                //print the value
+                System.out.printf("");
+            }   //end for loop columns
+        }   //end for loop rows
+    }  //end print2dArray
+}   //end class
--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,4 @@
-import java.awt.*;
+public class Test
+{
 
-public class Test {
-    public static void main (String[] args)
-    {
-        int x = 1;
-        int y = 2;
-
-        System.out.println(isEven(x));
-        System.out.println(isEven(y));
-        System.out.println(isOdd(x));
-        System.out.println(isOdd(y));
-        System.out.println(isOdd2(x));
-        System.out.println(isOdd2(y));
-        System.out.println(isOdd3(x));
-        System.out.println(isOdd3(y));
-    }
-
-    public static boolean isEven(int x){
-        if(x%2==0)
-            return true;
-        else
-            return false;
-    }
-
-    public static boolean isOdd(int x){
-        while(x != 1 && x != 0){
-            x = x/2;
-        }
-        return x==1;
-    }
-    public static boolean isOdd2(int x){
-        if(x%2==1)
-            return true;
-        else
-            return false;
-    }
-    public static boolean isOdd3(int x){
-        return x%2==1;
-    }
 }
