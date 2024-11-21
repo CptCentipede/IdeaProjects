9e2a4331666ed2cf208625e4bfd1c9a5577582d6
Miles <milesgriffin63@gmail.com>
2024-11-21 14:53:51 -0700

    test

---
+++ src/ClientStarter.java
@@ -0,0 +1,9 @@
+public class ClientStarter
+{
+	public static void main(String[] args)
+	{
+		Client c;
+		c = new Client();
+		c.runClient();
+	}
+}
---
+++ src/ServerStarter.java
@@ -0,0 +1,9 @@
+public class ServerStarter
+{
+	public static void main(String[] args)
+	{
+		Server my_server = new Server();
+
+		my_server.runServer();
+	}
+}
